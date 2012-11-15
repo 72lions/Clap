e5c1555366ad41b2d1782d6c94044558c53d0575
Rickard Strand <rickard.strand@gmail.com>
2012-11-16 00:38:57 +0100

    Broke out generic functionality from platform to Foreground

--- src/js/platform.js
+++ src/js/platform.js
@@ -1,22 +1,9 @@
 Crafty.c('Platform', {
 
-  _speed: 2,
-
   init: function() {
-    this.requires('Renderable')
+    this.requires('Foreground')
         .spriteName('platform')
-        .attr({x: Crafty.viewport.width, y: 584})
-        .bind('EnterFrame', function() {
-          this.x = this.x - this._speed;
-          if (this.x + this.w < 0) {
-            this.destroy();
-          }
-        });
-  },
-
-  speed: function(spd) {
-    this._speed = spd;
-    return this;
+        .attr({x: Crafty.viewport.width, y: 584});
   }
 
 });
